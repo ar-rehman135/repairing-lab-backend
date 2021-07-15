d4542d1193727a4689a257adaff6a83826853a63
Being-umer <beingumer785@gmail.com>
2021-07-15 12:47:23 +0500

    Update settings.py

--- repairshop/repairshop/settings.py
+++ repairshop/repairshop/settings.py
@@ -137,12 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 #media
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
